9c56aee18ff7458996301f73442acf4a91dc1c9f
Hans <hans.darmstadt-belanger1@uqac.ca>
2020-03-10 09:05:34 -0400

    étape 6 finie

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.SpannedString;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -13,14 +15,29 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
-private Switch monSwitch;
+    private Switch monSwitch;
+    private boolean layoutsDifferents = true;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView prenom = findViewById(R.id.txtPrenomCitoyen);
+        TextView nomFamille = findViewById(R.id.txtNomCitoyen);
+        TextView genre = findViewById(R.id.txtSexe);
+        ImageView signature = findViewById(R.id.signature);
 
         monSwitch = (Switch)findViewById(R.id.genderSwitch);
         monSwitch.setOnCheckedChangeListener(this);
+
+        if (savedInstanceState != null)
+        {
+            prenom.setText(savedInstanceState.getString("Prenom"));
+            nomFamille.setText(savedInstanceState.getString("NomFamille"));
+            genre.setText(savedInstanceState.getString("Genre"));
+            signature.setVisibility(savedInstanceState.getInt("SignatureVisible"));
+        }
     }
 
     @Override
@@ -41,9 +58,10 @@ private Switch monSwitch;
         EditText editNomFamille = findViewById(R.id.edtxtNom);
         TextView nomFamille = findViewById(R.id.txtNomCitoyen);
 
-        nomFamille.setText(editNomFamille.getText());
 
+        nomFamille.setText(editNomFamille.getText());
         prenom.setText(editPrenom.getText());
+
 
     }
 
@@ -61,6 +79,24 @@ private Switch monSwitch;
 
         }
 
+
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        TextView prenom = findViewById(R.id.txtPrenomCitoyen);
+        TextView nomFamille = findViewById(R.id.txtNomCitoyen);
+        TextView genre = findViewById(R.id.txtSexe);
+        ImageView signature = findViewById(R.id.signature);
+
+
+
+        outState.putString("Prenom", (String)prenom.getText().toString());
+        outState.putString("NomFamille", (String)nomFamille.getText().toString());
+        outState.putString("Genre", (String)genre.getText().toString());
+        outState.putInt("SignatureVisible", signature.getVisibility());
 
     }
 }
